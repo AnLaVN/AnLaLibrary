b67379b685d7e5b6b1a96829d63861b745e5df78
Bình An <binhan29122003@gmail.com>
2022-10-04 18:04:16 +0700

    Add files via upload

---
+++ AnLaLibrary_EN/src/com/AnLa/UI/AvatarPanel.java
@@ -0,0 +1,35 @@
+package com.AnLa.UI;
+// Make By Bình An || AnLaVN || KatoVN
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
+
+public class AvatarPanel extends javax.swing.JPanel{
+    private BufferedImage image;
+    public void setPic(String filePath) {
+        try { 
+            File file = new File(filePath.replaceAll("\\\\", "/"));
+            file.setExecutable(true);
+            file.setReadable(true);
+            file.setWritable(true);
+            image = ImageIO.read(file);
+        }
+        catch (IOException ex) {  System.out.println(ex); }
+        repaint();    
+    }
+    public AvatarPanel(){}
+    @Override protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        int circleDiameter = Math.min(getWidth(),getHeight());
+        Ellipse2D.Double circle = new Ellipse2D.Double(0,0,circleDiameter,circleDiameter);
+        g2.setClip(circle);
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+    }
+    @Override public void setCursor(Cursor cursor)      {   super.setCursor(cursor);}
+    @Override public void setLayout(LayoutManager mgr)  {   super.setLayout(mgr);   }
+}
---
+++ AnLaLibrary_VN/src/com/AnLa/HASH/SHA256.java
@@ -0,0 +1,30 @@
+package com.AnLa.HASH;
+// Make By Bình An || AnLaVN || KatoVN
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA256 {
+
+    /** Sử dụng phương thức này để mã hóa một chuỗi gốc.
+     * @param strToEncrypt là một chuỗi cần mã hóa. <br>
+     * @return mã băm SHA256 của chuỗi gốc.
+     */
+    public static String Encrypt(final String strToEncrypt) {
+        try {
+            final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            final byte[] hash = digest.digest(strToEncrypt.getBytes("UTF-8"));
+            final StringBuilder hexString = new StringBuilder();
+            for (int i = 0; i < hash.length; i++) {
+                final String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1) {    hexString.append('0');  }
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (UnsupportedEncodingException | NoSuchAlgorithmException ex) {
+            ex.toString();
+        } return null;
+    }
+    
+}
