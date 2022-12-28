f6cf67a1815fb89b7142b519e066efa84c7aee20
Bình An <binhan29122003@gmail.com>
2022-12-28 09:28:46 +0700

    Releases 3.61.21

--- AL-Library_Example/src/NET/ExampleCloud.java
+++ AL-Library_Example/src/NET/ExampleCloud.java
@@ -6,7 +6,7 @@ import com.AnLa.NET.Cloud;
 public class ExampleCloud {
 
     public static void main(String[] args) {
-        Cloud cloud = new Cloud("cloudinary://yoururlapi");//setup your cloudinary api
+        Cloud cloud = new Cloud("cloudinary://yoururlapi"); //setup your cloudinary api
         String url = cloud.FileUpload("myImage.png");   //get url after upload image file
         System.out.println("Link of my file: " + url);  //print url
         //cloud.FileDeleteByURL(url);                   //delete file using url
