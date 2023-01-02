eeda2d2af5501267d990d5ab1292ad8bd4acc7a0
Bình An <binhan29122003@gmail.com>
2023-01-02 14:17:22 +0700

    Releases 3.62.1410

--- AL-Library_Example/src/NET/ExampleDocNet.java
+++ AL-Library_Example/src/NET/ExampleDocNet.java
@@ -2,13 +2,12 @@ package NET;
 // Make By Bình An || AnLaVN || KatoVN
 
 import com.AnLa.NET.DocNet;
-
+import java.io.IOException;
 
 public class ExampleDocNet {
 
-    public static void main(String[] args) {
-        DocNet docnet = new DocNet("https://raw.githubusercontent.com/AnLaVN/AL-Library/Releases/LICENSE"); //set address of network document
+    public static void main(String[] args) throws IOException {
+        DocNet docnet = new DocNet("https://raw.githubusercontent.com/AnLaVN/AL-Library/Releases/LICENSE.md"); //set address of network document
         System.out.println(docnet.readAllLine());   //read all line in network document
     }
-    
 }
--- AL-Library_Example/src/OpenAI/ExampleChatGPT.java
+++ AL-Library_Example/src/OpenAI/ExampleChatGPT.java
@@ -7,8 +7,8 @@ import com.AnLa.OpenAI.ChatGPT;
 public class ExampleChatGPT {
 
     public static void main(String[] args) {
-        ChatGPT cgpt = new ChatGPT("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");  //connect to OpenAI service using API key
-        String Result = cgpt.Chat("Who is mr bean?", ChatGPT.Model.Davinci, 128); //get result from request, calling model Davinci with max 128 token
+        ChatGPT cgpt = new ChatGPT("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");  //connect to OpenAI service using API key
+        String Result = cgpt.Chat("How to install live server for old ?", ChatGPT.Model.Davinci, 128); //get result from request, calling model Davinci with max 128 token
         System.out.println(Result); //print result
     }
     
