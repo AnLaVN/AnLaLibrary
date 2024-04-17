2f9a11505dc9318dd259ba1a23e451b93a5d3bb7
Bình An <binhan29122003@gmail.com>
2024-04-17 15:19:26 +0700

    Update ExampleLicense.java

--- AL-Library_Example/src/example/anlavn/net/ExampleLicense.java
+++ AL-Library_Example/src/example/anlavn/net/ExampleLicense.java
@@ -4,8 +4,11 @@ package example.anlavn.net;
 import anlavn.net.License;
 
 public class ExampleLicense {
-    static { // Check the license authority before run code. Rhe program will continue if enough permissions. Else stop/delete the program.
-        License.check("https://github.com/AnLaVN/LICENSE/raw/Releases/AL-Library_License.properties");
+    
+    static { 
+        // Check the license authority before run code. 
+        // The program will continue if enough permissions. Else stop/delete the program.
+        License.check("&glwyD16e6JTgk6XuNYEj&D9WzOfqTMaJsmRNZ4+LNGIfSXH6QBbiYKVh/9a+SqDk=");
     }
 
     public static void main(String[] args) {
