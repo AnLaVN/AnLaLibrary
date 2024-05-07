49825e17ee85a13666d3f3a606717362d67f199b
Bình An <binhan29122003@gmail.com>
2024-05-08 04:56:49 +0700

    Update YGPT

--- YGPT/app.js
+++ YGPT/app.js
@@ -1,3 +1,2 @@
-var app = angular.module("YGPT_Demo",["ngSanitize"]);
+var app = angular.module("YGPT_Demo",[]);
 // Tên biến phải là "app" - The variable name must be "app"
-// Bao gồm mô-đun "ngSanitize" - Include "ngSanitize" module
